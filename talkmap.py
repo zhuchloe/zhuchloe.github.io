ace89ce97519146789c75293d7f7ebc491d668e6
Chloe Zhu <qz124@duke.edu>
2026-04-19 20:54:36 -0400

    upd

--- talkmap.py
+++ talkmap.py
@@ -1,56 +1,47 @@
-# Leaflet cluster map of talk locations
+
+
+# # Leaflet cluster map of talk locations
 #
-# Run this from the _talks/ directory, which contains .md files of all your
-# talks. This scrapes the location YAML field from each .md file, geolocates it
-# with geopy/Nominatim, and uses the getorg library to output data, HTML, and
-# Javascript for a standalone cluster map. This is functionally the same as the
-# #talkmap Jupyter notebook.
-import frontmatter
+# (c) 2016-2017 R. Stuart Geiger, released under the MIT license
+#
+# Run this from the _talks/ directory, which contains .md files of all your talks. 
+# This scrapes the location YAML field from each .md file, geolocates it with
+# geopy/Nominatim, and uses the getorg library to output data, HTML,
+# and Javascript for a standalone cluster map.
+#
+# Requires: glob, getorg, geopy
+
 import glob
 import getorg
 from geopy import Nominatim
-from geopy.exc import GeocoderTimedOut
 
-# Set the default timeout, in seconds
-TIMEOUT = 5
+g = glob.glob("*.md")
 
-# Collect the Markdown files
-g = glob.glob("_talks/*.md")
 
-# Prepare to geolocate
-geocoder = Nominatim(user_agent="academicpages.github.io")
+geocoder = Nominatim()
 location_dict = {}
 location = ""
 permalink = ""
 title = ""
 
-# Perform geolocation
+
 for file in g:
-    # Read the file
-    data = frontmatter.load(file)
-    data = data.to_dict()
+    with open(file, 'r') as f:
+        lines = f.read()
+        if lines.find('location: "') > 1:
+            loc_start = lines.find('location: "') + 11
+            lines_trim = lines[loc_start:]
+            loc_end = lines_trim.find('"')
+            location = lines_trim[:loc_end]
+                            
+           
+        location_dict[location] = geocoder.geocode(location)
+        print(location, "\n", location_dict[location])
 
-    # Press on if the location is not present
-    if 'location' not in data:
-        continue
 
-    # Prepare the description
-    title = data['title'].strip()
-    venue = data['venue'].strip()
-    location = data['location'].strip()
-    description = f"{title}<br />{venue}; {location}"
-
-    # Geocode the location and report the status
-    try:
-        location_dict[description] = geocoder.geocode(location, timeout=TIMEOUT)
-        print(description, location_dict[description])
-    except ValueError as ex:
-        print(f"Error: geocode failed on input {location} with message {ex}")
-    except GeocoderTimedOut as ex:
-        print(f"Error: geocode timed out on input {location} with message {ex}")
-    except Exception as ex:
-        print(f"An unhandled exception occurred while processing input {location} with message {ex}")
-
-# Save the map
 m = getorg.orgmap.create_map_obj()
-getorg.orgmap.output_html_cluster_map(location_dict, folder_name="talkmap", hashed_usernames=False)
+getorg.orgmap.output_html_cluster_map(location_dict, folder_name="../talkmap", hashed_usernames=False)
+
+
+
+
--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,22 @@
 var addressPoints = [
   [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
-    51.5074456,
-    -0.1277653
-  ],
-  [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
-    37.7879363,
-    -122.4075201
-  ],
-  [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
+    "Berkeley CA, USA",
     37.8708393,
-    -122.272863
+    -122.2728638
   ],
   [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
+    "London, UK",
+    51.5073219,
+    -0.1276473
+  ],
+  [
+    "San Francisco, California",
+    37.7792808,
+    -122.4192362
+  ],
+  [
+    "Los Angeles, CA",
+    34.0543942,
+    -118.2439408
   ]
 ];
